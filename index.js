c0c6ca92fc271c733905f0100e9282c71117c330
ttt-net <tenten102matsuyama@gmail.com>
2022-05-11 04:05:11 +0900

    Add keydown event

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var level = 0;
 
+$(document).keydown(function () {
+  if (level === 0) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+  }
+});
